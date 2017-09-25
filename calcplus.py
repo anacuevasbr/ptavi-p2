8aca920874d7a7df762da73f822d4584d3b421ad
Ana Cuevas Bravo <acuevas@gamma18.aulas.gsyc.urjc.es>
2017-09-25 17:04:04 +0200

    abre fichero

---
+++ calcplus.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import calcoohija
+
+if __name__== "__main__":
+
+    if len(sys.argv) != 2:
+        sys.exit("Input: python3 calcplus.py fichero")
+
+    open(sys.argv[1],"r")
+
