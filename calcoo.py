8e418123d08e4003fe68f4edbc8c4a1b8192ead7
Ana Cuevas Bravo <acuevas@gamma18.aulas.gsyc.urjc.es>
2017-09-25 16:02:44 +0200

    solo suma

--- calcoo.py
+++ calcoo.py
@@ -1,4 +1,31 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-print("HOla mundo")
+import sys
+
+class Calculadora:
+    
+    def __init__(self, valor1, valor2):
+        self.valor1=valor1
+        self.valor2=valor2
+
+    def suma(self):
+        return self.valor1 + self.valor2
+
+
+if __name__== "__main__":
+
+    if len(sys.argv) != 4:
+        sys.exit("Input: python3 calc.py operando1 operador operando2")
+
+    operador = sys.argv[2]
+    try:
+        Operar = Calculadora(float(sys.argv[1]), float(sys.argv[3]))
+
+    except ValueError:
+        sys.exit("Los operandos tienen que ser números")
+
+    resultado=Operar.suma()
+    print(resultado)
+
+    
