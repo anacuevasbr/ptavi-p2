c5dabe8f56a7819eeb40796b6215cf8f4e11f349
ana <anuskycuevas@gmail.com>
2017-09-30 13:34:31 +0200

    Completo

--- calcplusplus.py
+++ calcplusplus.py
@@ -12,6 +12,33 @@ if __name__== "__main__":
 
 
     with open(sys.argv[1], newline='') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            print(row)
+        Fich = csv.reader(f)
+        for Linea in Fich:
+            Operador=Linea[0]
+            Total=Linea[1]
+            Operandos=Linea[2:]
+
+            for Operando in Operandos:
+
+                try:
+                    Operar = calcoohija.CalculadoraHija(float(Total), float(Operando))
+                except ValueError:
+                    sys.exit("Los operandos tienen que ser números")
+                
+                if Operador=="suma":
+                    Total=Operar.suma()
+
+                elif Operador=="resta":
+                    Total=Operar.resta()
+
+                elif Operador=="multiplica":
+                    Total=Operar.multiplica()
+
+                elif Operador=="divide":
+                    Total=Operar.divide()
+                else:
+                    print("Las operaciones disponibles son suma, resta, multiplica y divide")
+
+            print(Total)
+
+                
