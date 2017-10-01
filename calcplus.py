158caaae5f10a2733c3931d04adb9f2a5f3ae0e0
ana <anuskycuevas@gmail.com>
2017-10-01 12:29:01 +0200

    corregido con pep8

--- calcplus.py
+++ calcplus.py
@@ -4,48 +4,47 @@
 import sys
 import calcoohija
 
-if __name__== "__main__":
+if __name__ == "__main__":
 
     if len(sys.argv) != 2:
         sys.exit("Input: python3 calcplus.py fichero")
 
-    
-    fich=open(sys.argv[1],"r")
-    lineas=fich.readlines()
-    
+    fich = open(sys.argv[1], "r")
+    lineas = fich.readlines()
+
     for linea in lineas:
-        operador=linea[:linea.find(',')]
-        linea=linea[linea.find(',')+1:-1]
-        Operandos=[]
-        
-        while (',' in linea)==1:
+        operador = linea[:linea.find(',')]
+        linea = linea[linea.find(',')+1:-1]
+        Operandos = []
+
+        while (',' in linea) == 1:
             Operandos.append(linea[:linea.find(',')])
-            linea=linea[linea.find(',')+1:]
+            linea = linea[linea.find(',')+1:]
         Operandos.append(linea)
-        
-        Total=Operandos[0]
-        Operandos=Operandos[1:]
-        
+
+        Total = Operandos[0]
+        Operandos = Operandos[1:]
+
         for Operando in Operandos:
 
             try:
-                Operar = calcoohija.CalculadoraHija(float(Total), float(Operando))
+                Operar = calcoohija.CalculadoraHija(float(Total),
+                                                    float(Operando))
             except ValueError:
                 sys.exit("Los operandos tienen que ser números")
-            
-            if operador=="suma":
-                Total=Operar.suma()
 
-            elif operador=="resta":
-                Total=Operar.resta()
+            if operador == "suma":
+                Total = Operar.suma()
 
-            elif operador=="multiplica":
-                Total=Operar.multiplica()
+            elif operador == "resta":
+                Total = Operar.resta()
 
-            elif operador=="divide":
-                Total=Operar.divide()
+            elif operador == "multiplica":
+                Total = Operar.multiplica()
+
+            elif operador == "divide":
+                Total = Operar.divide()
             else:
-                print("Las operaciones disponibles son suma, resta, multiplica y divide")
+                print("operaciones: suma, resta, multiplica y divide")
 
         print(Total)
-
