7790ecfa9458fa1e76f6a16957c230379527b3f0
ana <anuskycuevas@gmail.com>
2017-10-01 12:32:34 +0200

    corregido con pep8

--- calcplusplus.py
+++ calcplusplus.py
@@ -5,40 +5,38 @@ import sys
 import calcoohija
 import csv
 
-if __name__== "__main__":
+if __name__ == "__main__":
 
     if len(sys.argv) != 2:
         sys.exit("Input: python3 calcplus.py fichero")
 
-
     with open(sys.argv[1], newline='') as f:
         Fich = csv.reader(f)
         for Linea in Fich:
-            Operador=Linea[0]
-            Total=Linea[1]
-            Operandos=Linea[2:]
+            Operador = Linea[0]
+            Total = Linea[1]
+            Operandos = Linea[2:]
 
             for Operando in Operandos:
 
                 try:
-                    Operar = calcoohija.CalculadoraHija(float(Total), float(Operando))
+                    Operar = calcoohija.CalculadoraHija(float(Total),
+                                                        float(Operando))
                 except ValueError:
                     sys.exit("Los operandos tienen que ser números")
-                
-                if Operador=="suma":
-                    Total=Operar.suma()
 
-                elif Operador=="resta":
-                    Total=Operar.resta()
+                if Operador == "suma":
+                    Total = Operar.suma()
 
-                elif Operador=="multiplica":
-                    Total=Operar.multiplica()
+                elif Operador == "resta":
+                    Total = Operar.resta()
 
-                elif Operador=="divide":
-                    Total=Operar.divide()
+                elif Operador == "multiplica":
+                    Total = Operar.multiplica()
+
+                elif Operador == "divide":
+                    Total = Operar.divide()
                 else:
-                    print("Las operaciones disponibles son suma, resta, multiplica y divide")
+                    print("operaciones: suma, resta, multiplica y divide")
 
             print(Total)
-
-                
